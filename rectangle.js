caad0e89ae5c0fa03ef7cd5f77e8c9537f31a6a9
yangxi1998 <2462006777@qq.com>
2019-03-17 19:36:04 +0800

    矩形计算器

--- rectangle.js
+++ rectangle.js
@@ -1,11 +1,13 @@
-
+$(function() {
   console.log('hello');
-  var $width=$('#width'),$height=$("#height"),$btnCal=$('#calcute'),$perimeter=('#perimeter'),$area=$('#area');
+  var $width=$('#width'),$height=$("#height"),$btnCal=$('#calcute'),$perimeter=$('#perimeter'),$area=$('#area');
   $btnCal.click(function(){
     var w=Number($width.val()),
        h=Number($height.val());
   var p=2*(w+h),
     a=w*h;
+    console.log(p,a);
     $perimeter.val(p);
     $area.val(a);
   })
+})
