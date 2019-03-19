7afd57d84b6e54110e8976b0cc5532e284ca15ef
yangxi1998 <2462006777@qq.com>
2019-03-19 16:37:02 +0800

    v0.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,65 +13,16 @@ module.exports = function (grunt) {
       src: '*.css'
     },
     htmlhint: {
-      options: {
-        htmlhintrc: '.htmlhintrc'
-      },
-      src: '*.html'
-    }
-    mocha: {
-      test: {
-        src: ['test/index.html'],
-      },
-      options: {
-        run: true,
-        reporter: 'Spec'
+      html:{
+        options: {
+          htmlhintrc: '.htmlhintrc'
+        },
+        src: ['*.html']
       }
-    
-    
     }
-    htmlmin: {
-      options: {
-          collapseWhitespace: true,
-         preserveLineBreaks: true
-                                       
-      },
-    files: {
-        src: './index.html',
-        dest: 'dist/index.html'
-      }
-                 
-           },
-    cssmin: {
-      target:{
-        files:[{
-          src:['*.css'],
-          dest:'release/rectangle.css'
-        }
-      ]
-                           
-    }},
-    uglify: {
-          release:{
-            files: {
-                  'dist/rectangle.js': 'rectangle.js',
-                                                 
-                 }
-                              
-               }
-                           
-             }      
   });
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-mocha');
-  grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.registerTask('default', ['htmlhint', 'csslint', 'eslint']);
-  grunt.registerTask('default', ['mocha']);
-  grunt.registerTask('default', ['htmlmin']);
-  grunt.registerTask('default',['cssmin']);
-  grunt.registerTask('default',['uglify:release']);
-
 };
--- rectangle.js
+++ rectangle.js
@@ -3,10 +3,9 @@ $(function() {
   $btnCal.click(function(){
     var w=Number($width.val()),
         h=Number($height.val());
-   /* var p=2*(w+h),
-        a=w*h;*/
-    var r=new Rectangle(w,h); 
-    $perimeter.val(r.perimeter());
-    $area.val(r.area());
+    var p=2*(w+h),
+        a=w*h; 
+    $perimeter.val(p);
+    $area.val(a);
   });
 });
